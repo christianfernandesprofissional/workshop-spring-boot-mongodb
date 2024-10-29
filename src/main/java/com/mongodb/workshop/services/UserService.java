095a160cc9998421e5dd8706c7995167112d52b3
christianfernandesprofissional <christianfernandesprofissional@gmail.com>
2024-10-29 18:35:07 -0300

    Insercao de usuario com POST

--- src/main/java/com/mongodb/workshop/services/UserService.java
+++ src/main/java/com/mongodb/workshop/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.mongodb.workshop.domain.User;
+import com.mongodb.workshop.dto.UserDTO;
 import com.mongodb.workshop.repository.UserRepository;
 import com.mongodb.workshop.services.exceptions.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado") );
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
